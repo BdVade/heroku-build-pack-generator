faf5c75229af922451a001e5ce5458aa9eb721ba
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-06-28 01:40:10 +0100

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BuildPack(models):
+    name = models.CharField(max_length=500)
+    image = models.ImageField(null=True)
+    description = models.CharField(max_length=5000)
+    buildpack = models.FileField()
